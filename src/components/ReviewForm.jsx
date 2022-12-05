23a0c9ab31b70db6cb53b430312f57647ccbf019
Kailane Fonteles <lanef.fonteles@gmail.com>
2022-12-05 18:38:00 -0300

    Versão 1

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ReviewForm = () =>{
+    return(
+        <div>
+            <h2>Review Form</h2>
+        </div>
+    );
+};
+export default ReviewForm;
---
+++ src/components/Thanks.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Thanks = () =>{
+    return(
+        <div>
+            <h2>Thanks</h2>
+        </div>
+    );
+};
+export default Thanks;
